dffe4641100c354e2313a26f876a14e91777d9bd
textstack <textstack@protonmail.com>
2024-10-10 16:59:47 -0700

    improve movement

--- objects/char.gd
+++ objects/char.gd
@@ -1,9 +1,11 @@
 extends CharacterBody3D
 
-const SPEED = 15
-const JUMP_VELOCITY = 10
+const SPEED = 10
+const JUMP_VELOCITY = 7
 
 var sens = 0.2
+
+var can_jump = false
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -14,22 +16,31 @@ func _input(event: InputEvent) -> void:
 		rotate_y(deg_to_rad(-event.relative.x * sens))
 		$Camera3D.rotate_x(deg_to_rad(-event.relative.y * sens))
 
+func damp(from, to, delta):
+	return lerp(from, to, 1 - exp(-18 * delta))
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
+	if is_on_floor():
+		can_jump = true
+	else:
 		velocity += get_gravity() * delta
 		
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and can_jump:
+		can_jump = false
 		velocity.y = JUMP_VELOCITY
 	
 	var input_dir := Input.get_vector("Move_Left", "Move_Right", "Move_Forward", "Move_Back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
+	var vel = Vector3(0, 0, 0)
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		vel.x = direction.x * SPEED
+		vel.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x,0,SPEED)
-		velocity.z = move_toward(velocity.z,0,SPEED)
+		vel.x = 0
+		vel.z = 0
+	
+	velocity.x = damp(velocity.x, vel.x, delta)
+	velocity.z = damp(velocity.z, vel.z, delta)
 	
 	move_and_slide()
