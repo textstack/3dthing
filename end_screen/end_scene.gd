5cb071d4307342fb8931b84f0e2219a4b649753e
textstack <textstack@protonmail.com>
2024-10-28 19:48:33 -0700

    made quit button at the end quit game

--- end_screen/end_scene.gd
+++ end_screen/end_scene.gd
@@ -27,8 +27,7 @@ func _ready() -> void:
 func _on_restart_pressed() -> void:
 	Global.reset()
 	get_tree().change_scene_to_file("res://title_screen/title_background.tscn")
-	#get_tree().change_scene_to_file("res://areas/main.tscn")
 
 
 func _on_quit_pressed() -> void:
-	get_tree().change_scene_to_file("res://title_screen/title_background.tscn")
+	get_tree().quit()
